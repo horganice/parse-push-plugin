a2c44d9b1aaacfaec1f0ecc544f21c13dc11be69
Phawit <phawit.pw@gmail.com>
2018-09-02 21:40:32 +0700

    try fixbug gcm

--- scripts/afterAndroidPrepare.js
+++ scripts/afterAndroidPrepare.js
@@ -40,25 +40,25 @@ module.exports = function (context) {
 
   // Copy gcm sender id from config.xml into AndroidManifest
   // detect parse.com or parse-server mode
-  // var parseServerUrl = configXml.data.find('preference[@name="ParseServerUrl"]').get('value');
+  var parseServerUrl = configXml.data.find('preference[@name="ParseServerUrl"]').get('value');
 
-  // if (parseServerUrl.toUpperCase() !== "PARSE_DOT_COM") {
-  //   //opensource parse-server requires own GcmSenderId, so copy it from config.xml to AndroidManifest
-  //   var configXmlGcmIdNode = configXml.data.find('preference[@name="ParseGcmSenderId"]');
-  //   if (!configXmlGcmIdNode) {
-  //     console.error("ParseGcmSenderId is not set in config.xml");
-  //     return false;
-  //   }
+  if (parseServerUrl.toUpperCase() !== "PARSE_DOT_COM") {
+    //opensource parse-server requires own GcmSenderId, so copy it from config.xml to AndroidManifest
+    var configXmlGcmIdNode = configXml.data.find('preference[@name="ParseGcmSenderId"]');
+    if (!configXmlGcmIdNode) {
+      console.error("ParseGcmSenderId is not set in config.xml");
+      return false;
+    }
 
-  //   var manifestGcmIdNode = applicationNode.find('meta-data[@android:name="com.parse.push.gcm_sender_id"]');
+    var manifestGcmIdNode = applicationNode.find('meta-data[@android:name="com.parse.push.gcm_sender_id"]');
 
-  //   if (!manifestGcmIdNode) {
-  //     manifestGcmIdNode = new ET.Element('meta-data', { 'android:name': 'com.parse.push.gcm_sender_id' });
-  //     applicationNode.append(manifestGcmIdNode);
-  //   }
+    if (!manifestGcmIdNode) {
+      manifestGcmIdNode = new ET.Element('meta-data', { 'android:name': 'com.parse.push.gcm_sender_id' });
+      applicationNode.append(manifestGcmIdNode);
+    }
 
-  //   manifestGcmIdNode.set('android:value', 'id:' + configXmlGcmIdNode.get('value'));
-  // }
+    manifestGcmIdNode.set('android:value', 'id:' + configXmlGcmIdNode.get('value'));
+  }
 
 
   androidManifest.save();
